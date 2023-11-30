cd3aac661516b361412736470d1865119ca1737f
NIT3-PC12 <kjacobo587@gmail.com>
2023-11-30 12:35:06 +0800

    second

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import PokemonCard
 # Register your models here.
+@admin.site.register(PokemonCard)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("name", "rarity")
+    search_fields = ("name",)
